990c111473a876cbe2422a423fc64fdfb8686d7a
amitportnoy <amitportnoy@dalet.com>
2017-01-31 10:16:07 +0200

    demonstration of https://github.com/jspm/jspm-cli/issues/2226

--- jspm.config.js
+++ jspm.config.js
@@ -22,15 +22,16 @@ SystemJS.config({
     "npm:*.json"
   ],
   map: {
-    "angular-material": "npm:angular-material@1.1.2"
+    "angular": "npm:angular@1.6.1",
+    "angular-material": "npm:angular-material@1.1.1"
   },
   packages: {
-    "npm:angular-material@1.1.2": {
+    "npm:angular-material@1.1.1": {
       "map": {
-        "angular-animate": "npm:angular-animate@1.6.1",
-        "systemjs-plugin-css": "npm:systemjs-plugin-css@0.1.32",
         "angular": "npm:angular@1.6.1",
-        "angular-aria": "npm:angular-aria@1.6.1"
+        "angular-aria": "npm:angular-aria@1.6.1",
+        "systemjs-plugin-css": "npm:systemjs-plugin-css@0.1.32",
+        "angular-animate": "npm:angular-animate@1.6.1"
       }
     }
   }
